992c74de8f17a6c16f483f417f85d60e17eb6e2d
rfong <rachefo@gmail.com>
2021-06-09 10:15:29 -0700

    able to invoke Go-defined func from JS

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-// Example modified from https://tinygo.org/docs/guides/webassembly/
+// Load and run the WASM file in the browser.
+// Modified from https://tinygo.org/docs/guides/webassembly/
 
 const go = new Go(); // Defined in wasm_exec.js
 const WASM_URL = 'main.wasm';
+
+// Providing the environment object, used in WebAssembly.instantiateStreaming.
+// This part goes after "const go = new Go();" declaration.
+/*go.importObject.env = {
+	// This function can be used in Go.
+  'main.add': function(x, y) {
+    return x + y
+  }
+  // ... other functions
+}
+*/
+// A function to run once `wasm` is loaded
+function foo() {
+  // Calling a function exported from Go to JS
+  console.log('using Go-defined function to multiply two numbers:', wasm.exports.multiply(5, 3));
+}
 
 var wasm;
 
@@ -9,6 +26,7 @@ if ('instantiateStreaming' in WebAssembly) {
   WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then(function (obj) {
     wasm = obj.instance;
     go.run(wasm);
+		foo();
   })
 } else {
   fetch(WASM_URL).then(resp =>
@@ -17,6 +35,9 @@ if ('instantiateStreaming' in WebAssembly) {
     WebAssembly.instantiate(bytes, go.importObject).then(function (obj) {
       wasm = obj.instance;
       go.run(wasm);
+			foo();
     })
   )
 }
+
+
--- main.go
+++ main.go
@@ -6,4 +6,17 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello webassembly!")
+	//fmt.Println("main.go: using JS-defined function to add two numbers:", add(2, 3))
+}
+
+// This function is imported from JavaScript, as it doesn't define a body.
+// You should define a function named 'main.add' in the WebAssembly 'env'
+// module from JavaScript.
+//func add(x, y int) int
+
+// This function is exported to JavaScript, so can be called using
+// exports.multiply() in JavaScript.
+//export multiply
+func multiply(x, y int) int {
+	return x * y
 }
